ddbd86acf17c1e179f2d15bb84e8df5d8c67ed0e
marvelouss24 <masg28@lehigh.edu>
2024-10-28 15:21:20 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <msp430.h>
+
+int cal_add (int a, int b){
+    int result_add;
+    result_add = a+b;
+    return result_add;}
+
+
+int cal_sub (int a, int b){
+    int result_sub;
+    result_sub = a-b;
+    return result_sub;}
+
+int cal_mult (int a, int b){
+    int result_mult;
+    result_mult = a*b;
+    return result_mult;}
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+
+    int num1, num2;   // Declare two integer variables 'num1' and 'num2'.
+    int sum, sub, mult, mod, power;   // Declare variables to store the results of arithmetic operations.
+    float div;   // Declare a float variable 'div' to store the result of the division.
+
+    num1=10;
+    num2=5;
+
+
+    //Performs all arithmetic operations
+    sum = num1 + num2;   // Calculate the sum of 'num1' and 'num2'.
+    sub = num1 - num2;   // Calculate the difference between 'num1' and 'num2'.
+    //mult = num1 * num2;  // Calculate the product of 'num1' and 'num2'.
+    mult = cal_mult(num1, num2);
+    div = (float)num1 / num2;   // Calculate the division of 'num1' by 'num2' and cast the result to a float.
+    mod = num1 % num2;   // Calculate the modulus of 'num1' and 'num2'.
+    power = num1*num1*num1*num1*num1; //// Calculate powrr of 'num1' is 'num2'.
+
+    while(1);
+
+    return 0;   // Return 0 to indicate successful execution of the program.
+}
